e7e1b58c2e6c63ac6a82fecf38aa368a1ee83b14
tegajunior <chidiebereuzomahumble@gmail.com>
2022-03-11 14:01:27 +0100

    fixed https issue

--- src/default_axios.js
+++ src/default_axios.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
-const instance = axios.create({ baseURL: 'http://cipsmn-api.herokuapp.com/public' });
+const instance = axios.create({ baseURL: 'https://cipsmn-api.herokuapp.com/public' });
 
 export default instance
